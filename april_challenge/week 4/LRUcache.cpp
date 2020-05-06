6a7c93d404f07b3df9617e32708d6dc745912342
kunal <suri.kunal.222@gmail.com>
2020-05-06 11:57:38 +0530

    leetcode challenge

--- april_challenge/week 4/LRUcache.cpp
+++ april_challenge/week 4/LRUcache.cpp
@@ -1,81 +1,92 @@
 #include <iostream>
 #include <unordered_map>
 using namespace std;
-class LRUCache
+class Node
 {
 public:
-    // class LRUCache
-    // {
-    int key;
-    int val;
-    LRUCache *prev;
-    LRUCache *next;
-    // };
+    Node *next, *previous;
+    int key, value;
 
-    unordered_map<int, LRUCache *> map;
-    int cache_capacity;
-    LRUCache *head;
-    LRUCache *tail;
+    Node()
+    {
+        next = previous = nullptr;
+        key = value = -1;
+    }
+
+    Node(int key, int value)
+    {
+        this->key = key;
+        this->value = value;
+        next = previous = nullptr;
+    }
+};
+
+class LRUCache
+{
+private:
+    Node *head, *tail;
+    int maxCapacity, currentCapacity;
+    unordered_map<int, Node *> uMap;
+
+public:
     LRUCache(int capacity)
     {
-        this->cache_capacity = capacity;
-        head->next = tail;
-        tail->prev = head;
+        maxCapacity = capacity;
+        currentCapacity = 0;
+        head = new Node();
+        tail = new Node();
+
+        head->previous = tail;
+        tail->next = head;
     }
 
     int get(int key)
     {
-        int result = -1;
-        LRUCache *node = map[key];
-        if (node != nullptr)
-        {
-            result = node->val;
-            remove(node);
-            add(node);
-        }
-        return result;
+        if (uMap.find(key) == uMap.end())
+            return -1;
+        Node *node = uMap[key];
+        int value = node->value;
+
+        uMap.erase(key);
+        eraseNode(node);
+        addNode(key, value);
+
+        return value;
     }
 
     void put(int key, int value)
     {
-        LRUCache *node = map[key];
-        if (node != nullptr)
+        if (uMap.find(key) != uMap.end())
         {
-            remove(node);
-            node->val = value;
-            add(node);
+            currentCapacity--;
+            Node *currentNode = uMap[key];
+            uMap.erase(key);
+            eraseNode(currentNode);
         }
-        else
+        if (currentCapacity == maxCapacity)
         {
-            if (map.size() == cache_capacity)
-            {
-                map.erase(tail->prev->key);
-                remove(tail->prev);
-            }
-            LRUCache *new_node;
-            new_node->key = key;
-            new_node->val = value;
-
-            map[key] = new_node;
-            add(new_node);
+            uMap.erase(tail->next->key);
+            eraseNode(tail->next);
+            currentCapacity--;
         }
+        addNode(key, value);
+        currentCapacity++;
     }
 
-    void add(LRUCache *node)
+    void eraseNode(Node *node)
     {
-        LRUCache *head_next = head->next;
-        head->next = node;
-        node->prev = head;
-        node->next = head_next;
-        head_next->prev = node;
+        node->previous->next = node->next;
+        node->next->previous = node->previous;
+        delete node;
     }
 
-    void remove(LRUCache *node)
+    void addNode(int key, int value)
     {
-        LRUCache *node_prev = node->prev;
-        LRUCache *node_next = node->next;
-
-        node_prev->next = node_next;
-        node_next->prev = node_prev;
+        Node *node = new Node(key, value);
+        node->next = head;
+        node->previous = head->previous;
+        head->previous->next = node;
+        head->previous = node;
+        uMap[key] = node;
     }
 };
