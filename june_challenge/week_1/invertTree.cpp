fdccedfb5cc412e448651cccb83b378d82626afa
kunal <suri.kunal.222@gmail.com>
2020-06-02 02:17:58 +0530

    update

---
+++ june_challenge/week_1/invertTree.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution
+{
+public:
+    TreeNode *invertTree(TreeNode *root)
+    {
+        ios::sync_with_stdio(false);
+        cin.tie(NULL);
+        cout.tie(NULL);
+        if (root == nullptr)
+            return nullptr;
+        TreeNode *left = invertTree(root->left);
+        TreeNode *right = invertTree(root->right);
+        root->left = right;
+        root->right = left;
+        return root;
+    }
+};
