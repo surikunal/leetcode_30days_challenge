65125052674543f62edb1a97dbdc6c4fd85f6023
kunal <suri.kunal.222@gmail.com>
2020-06-02 23:35:57 +0530

    leetcode june challenge

---
+++ june_challenge/week_1/deleteNode.java
@@ -0,0 +1,15 @@
+public class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+    }
+}
+
+class Solution {
+    public void deleteNode(ListNode node) {
+        node.val = node.next.val;
+        node.next = node.next.next;
+    }
+}
