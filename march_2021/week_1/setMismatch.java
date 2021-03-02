bb18ec642b0d64717b3402f395bb0df98fdb82a8
surikunal <suri.kunal.222@gmail.com>
2021-03-02 23:25:05 +0530

    update

---
+++ march_2021/week_1/setMismatch.java
@@ -0,0 +1,37 @@
+class Solution {
+    public int[] findErrorNums(int[] nums) {
+        int[] freq = new int[nums.length + 1];
+        int dup = -1, miss = -1;
+        for (int i = 0; i < nums.length; i++) {
+            freq[nums[i]] += 1;
+        }
+        for (int i = 1; i <= nums.length; ++i) {
+            if (freq[i] == 2) {
+                dup = i;
+            } else if (freq[i] == 0) {
+                miss = i;
+            }
+        }
+        return new int[] {dup, miss};
+    }
+}
+
+//===================================================
+
+class Solution {
+    public int[] findErrorNums(int[] nums) {
+        int[] freq = new int[nums.length + 1];
+        int dup = -1, miss = -1;
+        for (int i = 0; i < nums.length; i++) {
+            freq[nums[i]] += 1;
+        }
+        for (int i = 1; i <= nums.length; ++i) {
+            if (freq[i] == 2) {
+                dup = i;
+            } else if (freq[i] == 0) {
+                miss = i;
+            }
+        }
+        return new int[] {dup, miss};
+    }
+}
